c4ce84d0014d3c2e753997111c116c88830e203e
SamirAk <44325916+SamirPS@users.noreply.github.com>
2018-10-20 22:32:14 +0200

    update du fichier
    Changement du nom de la base de donnée

--- model.py
+++ model.py
@@ -7,8 +7,9 @@ Created on Sat Oct 20 19:41:49 2018
 """
 import sqlite3
 Spe=[]#liste des spécialité
-connexion = sqlite3.connect('samir.db')#O ouvre la base de donnée
+connexion = sqlite3.connect('choixecole.db')#O ouvre la base de donnée
 curseur = connexion.cursor() 
+
 def specialité(table):
     """Nous revoie toutes les spécialité disponible sous forme d'une liste de tuples"""
     curseur.execute("SELECT Nom FROM Specialité")
@@ -16,5 +17,7 @@ def specialité(table):
     for specialite in specialite:
         table.append(specialite) #cherche les specialité dans la table spe
     return table
+
+
 def filtre(table,specialitéid):
     return 0
