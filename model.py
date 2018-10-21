301a936f26a9b450cb10ddb3ce37b8c9848ce079
SamirAk <44325916+SamirPS@users.noreply.github.com>
2018-10-21 11:53:06 +0200

    Update model.py

--- model.py
+++ model.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+c#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
 Created on Sat Oct 20 19:41:49 2018
@@ -7,19 +7,29 @@ Created on Sat Oct 20 19:41:49 2018
 """
 import sqlite3
 Spe=[]#liste des spécialité
+Ecole=[]#Liste des ecoles
 connexion = sqlite3.connect('choixecole.db')#O ouvre la base de donnée
 curseur = connexion.cursor() 
 
-def specialite(table):
-    
+def specialité(table):
     """Nous revoie toutes les spécialité disponible sous forme d'une liste de tuples"""
-    
     curseur.execute("SELECT Nom FROM Specialite")
-    listespecialite= curseur.fetchall()
-    for listespecialite in listespecialite:
-        table.append(listespecialite) #cherche les specialité dans la table spe
+    specialite = curseur.fetchall()
+    for specialite in specialite:
+        table.append(specialite) #cherche les specialité dans la table spe
     return table
 
+def filtre(table,specialiteid):
+     """Nous revoie toutes les ecoles disponible en fonction de la specialité choisi sous forme d'une liste de tuples"""
+     return table
 
-def filtre(table,specialitéid):
-    """renvoie la liste des ecoles en fonction de la specialite """
+
+Spe=specialité(Spe)
+print(Spe)
+
+specialiteid=int(input("donne le numero de la spécialite le premier terme de la liste est 0 \n"))
+while specialiteid>len(Spe):
+    print("bien jouer le bug ")
+    specialiteid=int(input("donne le numero de la spécialite le premier terme de la liste est 0 \n"))
+
+Ecole=filtre(Ecole,specialiteid)
