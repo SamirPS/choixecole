7d7365d8ac97cc5ad91b2f749adc31e5ae5546c7
SamirAk <44325916+SamirPS@users.noreply.github.com>
2018-10-22 15:06:16 +0200

    Update model.py

--- model.py
+++ model.py
@@ -16,7 +16,7 @@ def specialite(table):
     curseur.execute("SELECT Nom FROM Specialite")
     specialite = curseur.fetchall() #resultats de la commande
     for specialite in specialite:
-        table.append(specialite[0]) #apprend les specialité dans la table spe
+        table.append(specialite[0]) #apprend les specialité dans la liste table
     return table
 
 
@@ -32,14 +32,14 @@ Spe=specialite(Spe)
 for i in range(len(Spe)) : #on affiche les spécialite avec un numero pour faciliter l'usage du programme
     print(i+1,Spe[i])
     
-specialiteid=int(input("donne le numero de la spécialite \n"))
+specialiteid=int(input("donne le numero de la spécialite \n")) #Permet de connaitre la spe voulu par l'utilsateur 
 
 
 while specialiteid>len(Spe): #Eviter un bug
     print("bien jouer le bug ")
     specialiteid=int(input("donne le numero de la spécialite  \n"))
 
-Ecole=filtre(Ecole,specialiteid)
+Ecole=filtre(Ecole,specialiteid) # filtre les écoles en foction de la specialité
 
 for i in range(len(Ecole)): #affiche les écoles 
     print(Ecole[i])
