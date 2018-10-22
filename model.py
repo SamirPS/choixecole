a68b134b6daf705d0962f08241077be6f5e7a9a6
SamirAk <44325916+SamirPS@users.noreply.github.com>
2018-10-22 16:55:21 +0200

    Update model.py

--- model.py
+++ model.py
@@ -2,7 +2,6 @@
 # -*- coding: utf-8 -*-
 """
 Created on Sat Oct 20 19:41:49 2018
-
 @author: samir
 """
 import sqlite3
@@ -11,24 +10,25 @@ Ecole=[]#Liste des ecoles
 connexion = sqlite3.connect('choixecole.db')#On ouvre la base de donnée
 curseur = connexion.cursor() #execute les commandes sql
 
-def specialite(table):
+def specialite():
     """Nous revoie toutes les spécialité disponible sous forme d'une liste de tuples"""
     curseur.execute("SELECT Nom FROM Specialite")
     specialite = curseur.fetchall() #resultats de la commande
     for specialite in specialite:
-        table.append(specialite[0]) #apprend les specialité dans la table spe
-    return table
+        Spe.append(specialite[0]) #apprend les specialité dans la table spe
+    return Spe
 
 
-def filtre(table,specialiteid):
+def filtre(specialiteid):
     curseur.execute("SELECT Nom FROM EcoleSpe join EcoleS on EcoleSpe.IdEcole=EcoleS.id WHERE IdSpe=?",(specialiteid,))
     ecole = curseur.fetchall() #resultat de la commande
     for ecole in ecole:
-        table.append(ecole[0]) #appends les ecoles en fonction de la  specialité 
-    return table
+        Ecole.append(ecole[0]) #appends les ecoles en fonction de la  specialité 
+    return Ecole
 
 
-Spe=specialite(Spe)
+Spe=specialite()
+
 for i in range(len(Spe)) : #on affiche les spécialite avec un numero pour faciliter l'usage du programme
     print(i+1,Spe[i])
     
@@ -39,7 +39,7 @@ while specialiteid>len(Spe): #Eviter un bug
     print("bien jouer le bug ")
     specialiteid=int(input("donne le numero de la spécialite  \n"))
 
-Ecole=filtre(Ecole,specialiteid)
+Ecole=filtre(specialiteid)
 
 if len(Ecole)==0:# Si la liste a aucun élèment 
     print("Pas d'école trouvée en fonction des critéres")
