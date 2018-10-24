7f38e52edfecf10fe032e2adcb205ee95d99f384
SamirAk <44325916+SamirPS@users.noreply.github.com>
2018-10-24 19:51:41 +0200

    Update model.py

--- model.py
+++ model.py
@@ -5,8 +5,6 @@ Created on Sat Oct 20 19:41:49 2018
 @author: samir
 """
 import sqlite3
-Specialite=[]#liste des spécialité
-Ecole=[]#Liste des ecoles
 connexion = sqlite3.connect('choixecole.db')#On ouvre la base de donnée
 curseur = connexion.cursor() #execute les commandes sql
 
@@ -27,27 +25,3 @@ def filtre(specialiteid):
     for ecole in ecole:
         Ecole.append(ecole[0]) #appends les ecoles en fonction de la  specialité 
     return Ecole
-
-
-Specialite=renvoie_specialite()
-
-for i in range(len(Specialite)) : #on affiche les spécialite avec un numero pour faciliter l'usage du programme
-    print(i+1,Specialite[i])
-    
-specialiteid=int(input("donne le numero de la spécialite \n"))
-
-while specialiteid>len(Specialite) or specialiteid<1 : #Eviter un bug
-    specialiteid=int(input("donne le numero de la spécialite  compris entre 1 et 4 \n"))
-
-    
-Ecole=filtre(specialiteid)
-
-
-if len(Ecole)==0:# Si la liste a aucun élèment 
-    print("Pas d'école trouvée en fonction des critéres")
-else:
-    print("Voila les écoles : \n") #On affiche les écoles contenue dans la liste 
-    for i in range(len(Ecole)): 
-        print(Ecole[i])
-
-connexion.close() #on se deconnecte de la bdd
