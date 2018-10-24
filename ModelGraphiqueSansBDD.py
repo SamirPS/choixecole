c7f62b94edd5b1af593efe24be547ad03e36344d
SamirAk <44325916+SamirPS@users.noreply.github.com>
2018-10-24 19:39:24 +0200

    Update ModelGraphiqueSansBDD.py

--- ModelGraphiqueSansBDD.py
+++ ModelGraphiqueSansBDD.py
@@ -6,37 +6,44 @@ Created on Sat Oct 20 19:41:49 2018
 """
 from tkinter import Tk, StringVar, Label, Radiobutton
 from functools import partial
+
 class ChoixEcole:
+   
     
     def __init__(self):
-        Specialité=["Informatique","Sport","Maths"]#liste des spécialité
-        Ecole=[("TelecomSudParis","Informatique"),("TsP","Sport"),("Samsam","Sport"),("Samsam","Maths")]#Liste des ecoles
-        self.root = Tk() # On créer une fenetre
+        """Initialise l'application"""
+ 
+        # self.root représente la fenêtre dans la quelle se déroule notre application
+         
+        self.root = Tk() 
+        """Initialise les variables"""
+        
         self.var_choix = StringVar(self.root,)
-        label_ecole = Label(self.root, text='Ecole :' + self.var_choix.get())
+        self.Specialité=["Informatique","Sport","Maths"]#liste des spécialité
+        self.Ecole=[("TelecomSudParis","Informatique"),("TsP","Sport"),("Samsam","Sport"),("Samsam","Maths")]#Liste des ecoles
+        self.label_ecole = Label(self.root, text='Ecole :' + self.var_choix.get())
         
+        """On affiche les cases a cocher"""
         
-        def update_label(label):
-            """Met a jour le label en fonction de la case qui est cochée"""
-            Z=[]
-            text=""
-            for i in range (len(Ecole)):
-                Z=Ecole[i]
-                if str(self.var_choix.get())==str(Z[1]):
-                    text=text+"\n"+str(Z[0])
-            label.config(text="Ecole" + text)
-                    
-        
-        for i in range(len(Specialité)):
-            choix_1 = Radiobutton(self.root,variable=self.var_choix,text=str(Specialité[i]), value=Specialité[i],command=partial(update_label,label_ecole))
+        for i in range(len(self.Specialité)):
+            choix_1 = Radiobutton(self.root,variable=self.var_choix,text=self.Specialité[i], value=self.Specialité[i],command=partial(self.update_label))
             choix_1.grid(row=i+1, column=1,sticky="w")
        
-        label_ecole.grid(row=1, column=3,padx =40)
-        label_spe = Label(self.root, text='Specialité :' )
-        label_spe.grid(row=0, column=1)
+        self.label_ecole.grid(row=1, column=3,padx =40)
+        self.label_spe = Label(self.root, text='Specialité :' )
+        self.label_spe.grid(row=0, column=1)
     
        
         self.root.mainloop()
+    def update_label(self):
+            """Met a jour les écoles en fonction de la case qui est cochée"""
+            Z=[]
+            text=""
+            for i in range (len(self.Ecole)):
+                Z=self.Ecole[i]
+                if self.var_choix.get()==Z[1]:
+                    text=text+"\n"+Z[0]
+            self.label_ecole.config(text="Ecole" + text)
         
     
     
