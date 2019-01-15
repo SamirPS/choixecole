d27d321aba756b66d84a76604b21d8c7689cfdc8
SamirAk <44325916+SamirPS@users.noreply.github.com>
2019-01-15 21:36:34 +0100

    Add files via upload

---
+++ fichiertest/model.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 20 19:41:49 2018
+@author: samir
+"""
+
+"""On ouvre la base de données Sqlite3 et on s'y connecte """
+import sqlite3
+connexion = sqlite3.connect("choixecole.db")
+curseur = connexion.cursor()
+
+def renvoie_information(colonne,table):
+    """
+       LA fonction prend en variable la colonne et de la table de la base de donnée qui doivent être des str,
+       Elle renvoie les information contenue dans la base de donnée dans la liste Informationvoulue
+       Ex:renvoie_information("Nom","Specialite") renvoie les spécialites dans la liste Informationvoulue
+                                                                                                            """
+    colonne_peuimporte=("Region","Admission","Alternance")
+    informationvoulue=[resultat[0] for resultat in curseur.execute("SELECT "+colonne+" FROM "+table) ]
+    if colonne=="Nom":
+         informationvoulue=["Peu importe"]+informationvoulue
+    if colonne in colonne_peuimporte :
+        informationvoulue=["Peu importe"]+list(set(informationvoulue))
+    return informationvoulue
+
+def renvoie_coefficient():
+    """Donne la liste des concours et par la suite des dossier(pas implante encore) et donne les groupes et coefficient grâce a un 
+       dictonnaire """
+       
+    colonne="Groupe,Modelisation,Maths,Physique,SI,Informatique,Anglais,Francais,Bonification "
+    liste_concours=list(set(renvoie_information("Concours","Coefficient")))
+    concours={}
+    for nom in liste_concours:
+        concours[nom]={}
+        curseur.execute("Select "+colonne+" From Coefficient WHERE Concours="+'"'+nom+'"')
+        for resultat in curseur:
+            concours[nom][resultat[0]]=list(resultat[1:])
+    return concours
+
+
+def NoteCoefficient(coefficient,matiere):
+    """Renvoie la note coefficiente"""
+    note=[sum(coefficient[coeff]*matiere[coeff] for coeff in range (len(matiere))) ]
+    return note[0]
+
+def filtre(choix_utilisateur,groupe,note):
+
+    """
+    Construit la requete Sql et filtre les ecoles en fonction du choix de l'utilisateur
+    """
+    
+    conditions=[]
+    if note!=None:
+        conditions.append(("Points","<=",note))
+    if groupe!=None:
+        conditions.append(("Groupe","=",groupe))
+    if choix_utilisateur["Specialite"]!=None:
+        conditions.append(("Idspe","IN",choix_utilisateur["Specialite"]))
+    if choix_utilisateur["Alternance"]!=None:
+        conditions.append(("Alternance","IN",choix_utilisateur["Alternance"]))
+    if choix_utilisateur["Concours"]!=None :
+        conditions.append(("Admission","IN",choix_utilisateur["Concours"]))
+    if choix_utilisateur["Region"]!=None:
+        conditions.append(("Region","IN",choix_utilisateur["Region"]))
+    
+    
+    variables=tuple(conditions[i][2] for i in  range (len(conditions)) if conditions[i][1]!="IN")
+    requete="SELECT Nom,Admission,Commune FROM EcoleSpe join EcoleS on EcoleSpe.IdEcole=EcoleS.id WHERE "
+    
+    for i in range(len(conditions)):
+        if conditions[i][1]=="IN":
+            if len(conditions[i][2])==1:
+                requete+=conditions[i][0]+" "+conditions[i][1]+" "+str(conditions[i][2])[0:-2]+")"+" AND "
+            else: 
+                requete+=conditions[i][0]+" "+conditions[i][1]+" "+str(conditions[i][2])+" AND "
+                
+        else:
+            requete+=conditions[i][0]+conditions[i][1]+"? "+" AND "
+    
+    if conditions==[]:
+        requete=requete[0:len(requete)-6]
+    else :
+        requete=requete[0:len(requete)-4]
+        
+    ecoles=[ecole for ecole in curseur.execute(requete,variables)]
+    return ecoles
---
+++ fichiertest/viewBesnier.py
@@ -0,0 +1,264 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec 30 19:59:28 2018
+@author: samir
+"""
+from tkinter import Tk,StringVar,IntVar, Label,Entry,Listbox
+import model
+import tkinter.scrolledtext as tkscrolled
+
+class ChoixEcole:
+    def __init__(self):
+        
+        # Initialise l'application et change le titre et la positionne
+        self.root = Tk()
+        self.root.title("ChoixEcole")
+        self.root.resizable(False, False)
+       
+        # Initialise le widget de rendu
+        self.entry_ecole = tkscrolled.ScrolledText(self.root, width=30, height=10,)
+       
+        ########################################################################
+        #                        NOTES                                         #
+        ########################################################################
+        # On considère les notes entrées par l'utilisateur (sous forme de 
+        # "StringVar"). À chaque modification d'une de ces variables, on met
+        # tout à jour.
+
+        self.notes_vars = {
+            "maths": StringVar(self.root),
+            "francais": StringVar(self.root),
+            "SI":StringVar(self.root),
+            "Informatique":StringVar(self.root),
+            "Anglais":StringVar(self.root),
+            "Francais":StringVar(self.root)
+        }
+        for var in self.notes_vars.values():
+            var.trace('w', self.update)
+
+        # self.notes représente soit une erreur de saisie des notes (avec None)
+        # soit un dictionnaire "matière -> note(float)".
+        self.notes = None
+
+
+        ########################################################################
+        #                        CHOIX                                         #
+        ########################################################################
+        # Même idée que plus haut.
+        
+        self.choix = {
+            "Region": None,
+            "Specialite": None,
+            "Alternance": None,
+            "Concours": None,
+            "Année":None
+        }
+        
+        
+        ########################################################################
+        #                 RENDU FORMULAIRE NOTES                               #
+        ########################################################################
+        
+        Label(
+            self.root,
+            text="Moyenne en maths"
+        ).grid(row=1, column=1)
+        Entry(
+            self.root,
+            textvariable=self.notes_vars["maths"]
+        ).grid(row=2, column=1)
+
+        Label(
+            self.root,
+            text="Moyenne en français"
+        ).grid(row=3, column=1)
+        Entry(
+            self.root,
+            textvariable=self.notes_vars["francais"]
+        ).grid(row=4, column=1)
+        
+        Label(
+            self.root,
+            text="Moyenne en Si"
+        ).grid(row=5, column=1)
+        Entry(
+            self.root,
+            textvariable=self.notes_vars["SI"]
+        ).grid(row=6, column=1)
+        
+        Label(
+            self.root,
+            text="Moyenne en Informatique"
+        ).grid(row=7, column=1)
+        Entry(
+            self.root,
+            textvariable=self.notes_vars["Informatique"]
+        ).grid(row=8, column=1)
+        
+        Label(
+            self.root,
+            text="Moyenne en Anglais"
+        ).grid(row=9, column=1)
+        Entry(
+            self.root,
+            textvariable=self.notes_vars["Anglais"]
+        ).grid(row=10, column=1)
+        
+        Label(
+            self.root,
+            text="Moyenne en Francais"
+        ).grid(row=11, column=1)
+        Entry(
+            self.root,
+            textvariable=self.notes_vars["Francais"]
+        ).grid(row=12, column=1)
+       
+         ########################################################################
+        #                 RENDU FORMULAIRE choix                               #
+        ########################################################################
+        self.specialite=Listbox(
+                self.root,
+                selectmode='multiple',
+                exportselection=0, 
+                width=20, 
+                height=10)
+        self.Region=Listbox(
+                self.root,
+                selectmode='multiple',
+                exportselection=0, 
+                width=20, 
+                height=10)
+        self.Alternance=Listbox(
+                self.root,
+                selectmode='multiple',
+                exportselection=0, 
+                width=20, 
+                height=10)
+        self.Concours=Listbox(
+                self.root,
+                selectmode='multiple',
+                exportselection=0, 
+                width=20, 
+                height=10)
+        self.Année=Listbox(
+                self.root,
+                exportselection=0, 
+                width=20, 
+                height=10)
+        
+        self.specialite.grid(
+                row=1,
+                column=6,
+                rowspan=10,
+                padx=10)
+        self.Region.grid(
+                row=1,
+                column=7,
+                rowspan=10,
+                padx=10)
+        self.Alternance.grid(
+                row=1,
+                column=8,
+                rowspan=10,
+                padx=10)
+        self.Concours.grid(
+                row=1,
+                column=9,
+                rowspan=10,
+                padx=10)
+        self.Année.grid(
+                row=1,
+                column=10,
+                rowspan=10,
+                padx=10)
+        
+         ########################################################################
+        #                 Insertion des données                               #
+        ########################################################################
+        for specialite in model.renvoie_information("Nom","Specialite"):
+                self.specialite.insert("end",specialite)
+        
+        for Region in model.renvoie_information("Region","EcoleS"):
+            self.Region.insert("end",Region)
+        
+        for Alternance in model.renvoie_information("Alternance","EcoleSpe"):
+             self.Alternance.insert("end",Alternance)
+        
+        for Concours in model.renvoie_information("Admission","EcoleS"):
+            self.Concours.insert("end",Concours)
+        
+        for annee in ["3/2","5/2"]:
+             self.Année.insert("end",annee)
+        
+
+        self.entry_ecole.grid(row=1,column=20,rowspan=10) 
+        
+        self.update()
+        self.root.mainloop()
+
+    def valide_maj_notes(self):
+        try:
+            notes = {}
+            for nom_matiere, note_var in self.notes_vars.items():
+                note_float = float(note_var.get())
+                if note_float > 20 or note_float < 0:
+                    raise ValueError()
+
+                notes[nom_matiere] = note_float
+            self.notes = notes
+
+        except ValueError:
+            # Une erreur est survenue lors de la conversion des notes
+            self.notes = None
+
+    def maj_choix(self):
+        self.choix={"Specialite":self.specialite.curselection(),
+                    "Region":tuple(self.Region.get(i) for i in self.Region.curselection()),
+                    "Concours":tuple(self.Concours.get(i) for i in self.Concours.curselection()),
+                    "Alternance":tuple(self.Alternance.get(i) for i in self.Alternance.curselection()),
+                    "Année":self.Année.curselection()}
+        
+        for cle in self.choix:
+            if self.choix[cle]==():
+                self.choix[cle]=None
+                
+            
+        
+    def update(self, *inutile):
+        self.valide_maj_notes()
+        self.maj_choix()
+        self.affichage()
+
+        
+    def affichage(self):
+        # Active le champs Ecole et supprime ce qu'il y avait écrit avant
+        self.entry_ecole.configure(state="normal")
+        self.entry_ecole.delete(0.7,'end')
+     
+        text_affiche = "" 
+        
+        if self.notes == None:
+            text_affiche = "Erreur lors de la saisie des notes."
+        else:       
+            ecoles = model.filtre(self.choix, None, calculer_moyenne(self.notes))
+            for ecole in ecoles:
+                text_affiche += (
+                    "\n" 
+                    + ecole[0] + " "
+                    + ecole[1] + " "
+                    + ecole[2]
+                )
+        
+
+        self.entry_ecole.insert(0.7, text_affiche)
+        self.entry_ecole.configure(state="disabled")
+         
+
+def calculer_moyenne(notes):
+    """Fonction fictive de calcul de moyenne (devrait être dans model.py)."""
+    return notes["maths"] * 50 + notes["francais"] * 10
+
+
+if __name__ == '__main__':
+    ChoixEcole()
