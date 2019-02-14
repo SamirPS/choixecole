c26c55bcac3e2e527abff654ae3e5dbfb9ea8cd7
RobinChaussemy <47593194+RobinChaussemy@users.noreply.github.com>
2019-02-14 13:41:30 +0100

    Add files via upload

---
+++ RobinAndSamir/untitled29.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 12 14:19:23 2019
+
+@author: robinchaussemy
+"""
+import openpyxl
+import sqlite3
+
+connexion = sqlite3.connect("choixecole.db")
+curseur = connexion.cursor()
+
+
+""" Retourne la taille du fichier et les valeurs"""
+    
+book = openpyxl.load_workbook('Classeur1.xlsx')
+sheet = book.get_sheet_by_name('Feuil1')
+r = sheet.max_row
+c = sheet.max_column
+def ecole():
+    for i in range(3,r):
+        t2=[]
+        for j in range(1, c+1):
+            temp=sheet.cell(row=i, column=j).value
+            t2.append(temp)
+        curseur.execute("INSERT INTO EcoleS (Acronyme,Nom,Groupe,Commune,CommuneAnnexe,Region,Admission,Statut,Points,PrixB,PrixNB) VALUES  (?,?,?,?,?,?,?,?,?,?,?)",(t2[0],t2[1],"?",t2[2],t2[4],t2[3],t2[5],t2[6],0,t2[38],t2[39]))
+    connexion.commit()
+def spe():
+    t=[]
+    for i in range(11,35):
+        temp=tuple(sheet.cell(row=2,column=i).value)
+        t.append(temp)
+        curseur.execute("INSERT INTO spe (Nomspe) VALUES (?)",t[0])
+    
+        
+spe()
+connexion.commit()
